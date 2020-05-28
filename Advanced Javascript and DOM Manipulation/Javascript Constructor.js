3c17b9a9b7d50518f55ad39ccb15cccc0399ccf1
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-28 20:47:22 +0530

    Creating objects using constructors and initialising them.

--- Advanced Javascript and DOM Manipulation/Javascript Constructor.js
+++ Advanced Javascript and DOM Manipulation/Javascript Constructor.js
@@ -1,1 +1,13 @@
+// Creating a constructor function 
+//in which, unlike camel notations, first letter of the name of function is always capital.
 
+function BellBoy (name, age, hasWorkPermit, languages) {
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+}
+
+// Initialising the object
+
+var bellBoy1 = new BellBoy("Timmy", 19, true, ["French", "English"]);
