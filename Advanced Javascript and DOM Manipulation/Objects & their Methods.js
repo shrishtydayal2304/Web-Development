bc4a11ddd39d2274b467d55feb492fc88a885060
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-28 22:04:29 +0530

    Update Objects & their Methods.js

--- Advanced Javascript and DOM Manipulation/Objects & their Methods.js
+++ Advanced Javascript and DOM Manipulation/Objects & their Methods.js
@@ -1,1 +1,54 @@
+// classical approach
 
+var bellBoy1 = {
+  name: "Timmy",
+  age: 19,
+  hasWorkPermit: true,
+  languages: ["French", "English"],
+  moveSuitcase: function() {
+    alert("May i take your suitcase?");
+    pickUpSuitcase();
+    move();
+  }
+}
+
+// Call method
+bellBoy1.moveSuitcase();
+
+// Using Constructor function
+
+function BellBoy (name, age, hasWorkPermit, languages) {
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+  this.moveSuitcase = function(){
+    alert("May i take your suitcase?");
+    pickUpSuitcase();
+    move();
+  }
+}
+
+// calling method
+BellBoy.moveSuitcase();
+
+
+// .............................................................................................................................
+
+// for that audio file which we've used in drum kit. That is similar to this thing.
+
+// initialising the audio object
+var tom1 = new Audio("sounds/tom-1.mp3);  
+tom1.play();               // calling the method
+
+// this audio constructor function might look like :
+
+function Audio (fileLocation) {
+  this.fileLocation = fileLocation;
+  this.play = function() {
+    //Tap into the audio hardware
+    //Check the file at fileLocation exists
+    //Check the file at fileLocation is a sound file
+    //Play the file at fileLocation
+  }
+}
