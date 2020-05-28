26621d6697eca10944c7621e4795e550189a3376
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-28 20:50:53 +0530

    Creating objects without constructor (Classical approach)

--- Advanced Javascript and DOM Manipulation/Javascript Objects.js
+++ Advanced Javascript and DOM Manipulation/Javascript Objects.js
@@ -1,3 +1,6 @@
+/* Creating objects without using constructor going through classical approach in which
+ we might have to write same snippet for other similar BellBoy objects. */
+
 var bellBoy1 = {
   name: "Timmy",
   age: 19,
