a731456b250e725d7c197c78cc8ac236ef135228
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-05-30 22:22:56 +0530

    Adding another page.

--- my-express-server/Creating my First server/server.js
+++ my-express-server/Creating my First server/server.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res){
   res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/contact", function(req, res){
+  res.send("Contact me at ojasvi@gmail.com");
+});
+
 app.listen(3000, function(){
   console.log("Server has started on port 3000");
 });
